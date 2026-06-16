c4e0f99e8e910bc46a78a0407ab41b8953b4f3ca
xuhao199747@gmail.com <xuhao199747@gmail.com>
2026-06-16 14:39:29 +0800

    Deploy with official GitHub Pages workflow

--- build-post.js
+++ build-post.js
@@ -4,7 +4,7 @@ const htmlPath = './dist/index.html';
 let htmlContent = fs.readFileSync(htmlPath, 'utf-8');
 
 const stylesLink = '<link rel="stylesheet" href="./styles.css" />';
-const mainCssLink = '<link rel="stylesheet" crossorigin href="/assets/main-';
+const mainCssLink = '<link rel="stylesheet" crossorigin href="./assets/main-';
 
 if (!htmlContent.includes(stylesLink)) {
   htmlContent = htmlContent.replace(mainCssLink, stylesLink + '\n    ' + mainCssLink);
