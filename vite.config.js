30cb85f10c667d96f8998bd22a729eb6a4bd8488
xuhao199747@gmail.com <xuhao199747@gmail.com>
2026-06-16 14:25:16 +0800

    Fix GitHub Pages build config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import { readFileSync, writeFileSync } from 'fs'
 
 export default defineConfig({
+  plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.json']
   },
