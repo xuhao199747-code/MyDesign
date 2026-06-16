af685edc6ca8fd1d430f4cadb2dfa7ebea211fa0
xuhao199747@gmail.com <xuhao199747@gmail.com>
2026-06-16 14:30:33 +0800

    Use ESM Vite config for Pages build

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { readFileSync, writeFileSync } from 'fs'
 
 export default defineConfig({
   plugins: [react()],
