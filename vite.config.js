52cefd4d70090391f1330a6c74fb487190a10ca8
xuhao199747@gmail.com <xuhao199747@gmail.com>
2026-06-16 14:19:24 +0800

    Fix build: remove unnecessary React plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import { readFileSync, writeFileSync } from 'fs'
 
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.json']
   },
